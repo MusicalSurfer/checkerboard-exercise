e7748c9ed22bf28014644a5189662ab4f354887f
DANIEL_GRADY <anakria977@gmail.com>
2023-12-13 14:35:21 -0500

    Finished basic checkerboard.

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-// Function to create a tile element
-function createTile() {
-    const tile = document.createElement('div');
-    tile.style.width = "12.5%";
-    tile.style.height = "12.5%";
-    tile.style.border = "1px solid #000"; // Just for visualization
-    return tile;
-}
+const container = document.createElement('div'); // Create a container.
+document.body.appendChild(container); // Append container to document body.
 
-//Function to create and append tiles to the container
-function createTiles(container, numTiles) {
-    for (let i = 0; i < numTiles; i++) {
-        const tile = createTile();
-        container.appendChild(tile);
+// Function that creates and appends desired amount of tiles in checkerboard patter to container.
+const createTiles = (container, numOfTiles) => {
+    let rows = Math.sqrt(numOfTiles); // Find how many rows are in given grid
+    let tilesPerRow = Math.sqrt(numOfTiles); // Assuming it's a square, find how many tiles will be in each row.
+    let rowComplete = false; // Flag to store if a row has been iterated through.
+
+    // For every row...
+    for (let i = 0; i < rows; i++) {
+        // For every tile...
+        for (let j = 0; j < tilesPerRow; j++) {
+            const tile = document.createElement('div'); // Create the tile
+            styleTiles(tile, j, rowComplete); // Apply styling in checkerboard pattern.
+            container.appendChild(tile); // Append finished tile to the container.
+        }
+        (!rowComplete) ? rowComplete = true : rowComplete = false; // Once row is complete, flip flag to change styling.
     }
 }
 
-// Function to set styles for the container
-function setContainerStyles(container) {
+// Helper function to set style for tiles in createTiles.
+const styleTiles = (tile, index, rowComplete) => {
+    tile.style.width = '12.5%';
+    tile.style.height = '12.5%';
+
+    // If flag is false, apply default styling, otherwise apply next row style.
+    if (!rowComplete) {
+        // If index is even apply red, otherwise apply black.
+        (index % 2 === 0) ? tile.style.backgroundColor = 'red' : tile.style.backgroundColor = 'black';
+    }
+    else {
+        // If index is odd apply red, otherwise apply black.
+        (index % 2 === 1) ? tile.style.backgroundColor = 'red' : tile.style.backgroundColor = 'black';
+    }
+}
+
+// Function to set style for container.
+const styleContainer = (container) => {
     container.style.display = 'flex';
     container.style.flexWrap = 'wrap';
     container.style.width = '800px';
     container.style.height = '800px';
-    container.style.border = '1px solid #000'; // Just for visualization
 }
 
-// Get the container element by ID
-const container = document.createElement('div');
-container.id = 'tileContainer';
-
-// Append the container to the body
-document.body.appendChild(container);
-
-// Set the styles for the container
-setContainerStyles(container);
-
-// Create and append the tiles to the container
-createTiles(container, 64);
+styleContainer(container); // Style container
+createTiles(container, 64); // Create desired tiles.

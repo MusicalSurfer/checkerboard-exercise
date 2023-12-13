e024c711a214c7a4b38d5192f16b87456ab1c045
DANIEL_GRADY <anakria977@gmail.com>
2023-12-13 16:06:40 -0500

    Used math.floor to condense code.

--- script.js
+++ script.js
@@ -4,34 +4,23 @@ document.body.appendChild(container); // Append container to document body.
 // Function that creates and appends desired amount of tiles in checkerboard patter to container.
 const createTiles = (container, numOfTiles) => {
     let rows = Math.sqrt(numOfTiles); // Find how many rows are in given grid
-    let tilesPerRow = Math.sqrt(numOfTiles); // Assuming it's a square, find how many tiles will be in each row.
-    let rowComplete = false; // Flag to store if a row has been iterated through.
 
-    // For every row...
-    for (let i = 0; i < rows; i++) {
-        // For every tile...
-        for (let j = 0; j < tilesPerRow; j++) {
-            const tile = document.createElement('div'); // Create the tile
-            styleTiles(tile, j, rowComplete); // Apply styling in checkerboard pattern.
-            container.appendChild(tile); // Append finished tile to the container.
-        }
-        (!rowComplete) ? rowComplete = true : rowComplete = false; // Once row is complete, flip flag to change styling.
+    for (let i = 0; i < numOfTiles; i++) {
+        const tile = document.createElement('div'); // Create the tile
+        styleTiles(tile, i, rows); // Apply styling in checkerboard pattern.
+        container.appendChild(tile); // Append finished tile to the container.
     }
 }
 
 // Helper function to set style for tiles in createTiles.
-const styleTiles = (tile, index, rowComplete) => {
+const styleTiles = (tile, index, rows) => {
     tile.style.width = '12.5%';
     tile.style.height = '12.5%';
-
-    // If flag is false, apply default styling, otherwise apply next row style.
-    if (!rowComplete) {
-        // If index is even apply red, otherwise apply black.
-        (index % 2 === 0) ? tile.style.backgroundColor = 'red' : tile.style.backgroundColor = 'black';
+    if ((index + Math.floor(index / rows)) % 2 === 0) {
+        tile.style.backgroundColor = 'red';
     }
     else {
-        // If index is odd apply red, otherwise apply black.
-        (index % 2 === 1) ? tile.style.backgroundColor = 'red' : tile.style.backgroundColor = 'black';
+        tile.style.backgroundColor = 'black';
     }
 }
 

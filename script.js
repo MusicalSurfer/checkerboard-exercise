ff83ba0807accf5540c5b9f96206580cfba04683
DANIEL_GRADY <anakria977@gmail.com>
2023-12-13 15:20:01 -0500

    First commit for gradient.

--- script.js
+++ script.js
@@ -3,28 +3,40 @@ document.body.appendChild(container); // Append container to document body.
 
 // Function that creates and appends desired amount of tiles in checkerboard patter to container.
 const createTiles = (container, numOfTiles) => {
-    for (let j = 0; j < numOfTiles; j++) {
-        const tile = document.createElement('div'); // Create the tile
-        styleTiles(tile, getRandomColor); // Apply styling to tiles.
-        container.appendChild(tile); // Append finished tile to the container.
+    let rows = Math.sqrt(numOfTiles); // Find how many rows are in given grid
+    let tilesPerRow = Math.sqrt(numOfTiles); // Assuming it's a square, find how many tiles will be in each row.
+    let rowComplete = false; // Flag to store if a row has been iterated through.
+    let firstRgbValue = [150, 120, 150];
+    let secondRgbValue = [170, 120, 150];
+    let firstRgb = 'rgb(' + secondRgbValue[0] + ', ' + secondRgbValue[1] + ', ' + secondRgbValue[2] + ')';
+    let secondRgb = 'rgb(' + firstRgbValue[0] + ', ' + firstRgbValue[1] + ', ' + firstRgbValue[2] + ')';
+
+    // For every row...
+    for (let i = 0; i < rows; i++) {
+        // For every tile...
+        for (let j = 0; j < tilesPerRow; j++) {
+            const tile = document.createElement('div'); // Create the tile
+            styleTiles(tile, j, rowComplete, firstRgb, secondRgb); // Apply styling in checkerboard pattern.
+            container.appendChild(tile); // Append finished tile to the container.
+        }
+        (!rowComplete) ? rowComplete = true : rowComplete = false; // Once row is complete, flip flag to change styling.
     }
 }
 
 // Helper function to set style for tiles in createTiles.
-const styleTiles = (tile, randomColorFunction) => {
+const styleTiles = (tile, index, rowComplete, firstRgb, secondRgb) => {
     tile.style.width = '12.5%';
     tile.style.height = '12.5%';
-    tile.style.backgroundColor = randomColorFunction();
-}
 
-// Helper function to get random hex color.
-const getRandomColor = () => {
-    let hexLetters = '0123456789ABSDEF';
-    let randomColor = '#';
-    for (let i = 0; i < 6; i++) {
-        randomColor += hexLetters[Math.floor(Math.random() * 16)];
+    // If flag is false, apply default styling, otherwise apply next row style.
+    if (!rowComplete) {
+        // If index is even apply red, otherwise apply black.
+        (index % 2 === 0) ? tile.style.backgroundColor = firstRgb : tile.style.backgroundColor = secondRgb;
     }
-    return randomColor;
+    else {
+        // If index is odd apply red, otherwise apply black.
+        (index % 2 === 1) ? tile.style.backgroundColor = 'red' : tile.style.backgroundColor = 'black';
+    }
 }
 
 // Function to set style for container.
